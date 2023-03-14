623728db643f072ccbb817a5f18b205705a06826
Oyinade3 <oyinadehillary@gmail.com>
2023-03-14 23:06:17 +0100

    made final adjustments to website

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//animation code
 const observer = new IntersectionObserver((entries)=>{
     entries.forEach(entry=>{
         console.log(entry);
@@ -11,7 +12,13 @@ const observer = new IntersectionObserver((entries)=>{
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach(hiddenElement => observer.observe(hiddenElement))
+const slideRight = document.querySelectorAll('.slideRight');
+slideRight.forEach(hiddenElement => observer.observe(hiddenElement))
+const slideLeft = document.querySelectorAll('.slideLeft');
+slideLeft.forEach(hiddenElement => observer.observe(hiddenElement))
 
+
+//code for hamburger nav menu on small screens
 const hamburger = document.querySelector('#hamburger');
 const navPages = document.querySelector('.pages');
 hamburger.addEventListener('click', ()=>{
